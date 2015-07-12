63ecbabc1c78b71b30d0d930cd432ba2fc1b15e4
Greg McNutt <greg.mcnutt@corp.vendio.com>
2015-07-11 18:57:33 -0700

    Xcode7, WatchOS2 beta3, iOS9, Swift 2.0
    Oh, and also add in accelerometer data...

--- ttl WatchKit Extension/InterfaceController.swift
+++ ttl WatchKit Extension/InterfaceController.swift
@@ -8,18 +8,22 @@
 
 import WatchKit
 import Foundation
-
+import CoreMotion
 
 class InterfaceController: WKInterfaceController {
 
     var arrivalDate: NSDate!
+    let mm = CMMotionManager()
     var timer = NSTimer()
     
     @IBOutlet weak var days: WKInterfaceLabel!
     @IBOutlet weak var hours: WKInterfaceLabel!
     @IBOutlet weak var minutes: WKInterfaceLabel!
     @IBOutlet weak var seconds: WKInterfaceLabel!
-    
+    @IBOutlet var x: WKInterfaceLabel!
+    @IBOutlet var y: WKInterfaceLabel!
+    @IBOutlet var z: WKInterfaceLabel!
+
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
 
@@ -29,7 +33,7 @@ class InterfaceController: WKInterfaceController {
 
         // Configure interface objects here.
     }
-
+   
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
@@ -42,6 +46,16 @@ class InterfaceController: WKInterfaceController {
             userInfo: nil,
             repeats: true)
         showTTL(timer)
+        
+        if (mm.accelerometerAvailable) {
+            mm.accelerometerUpdateInterval = 0.5
+            self.mm.startAccelerometerUpdatesToQueue(NSOperationQueue()) {
+                (data, error) in
+                self.x.setText(NSString(format: "%.2f", data!.acceleration.x) as String)
+                self.y.setText(NSString(format: "%.2f", data!.acceleration.y) as String)
+                self.z.setText(NSString(format: "%.2f", data!.acceleration.z) as String)
+            }
+        }
     }
 
     override func didDeactivate() {
@@ -52,23 +66,18 @@ class InterfaceController: WKInterfaceController {
     
     func showTTL(timer:NSTimer) {
         let now = NSDate()
-        let dayHourMinuteSecond: NSCalendarUnit =
-        .CalendarUnitDay    |
-            .CalendarUnitHour   |
-            .CalendarUnitMinute |
-            .CalendarUnitSecond
+        let dayHourMinuteSecond: NSCalendarUnit = [ .Day, .Hour, .Minute, .Second ]
         
         let difference = NSCalendar.currentCalendar().components(
             dayHourMinuteSecond,
             fromDate: now,
             toDate: arrivalDate,
-            options: nil)
+            options: [])
         
         self.days.setText(difference.day.description)
         self.hours.setText(difference.hour.description)
         self.minutes.setText(difference.minute.description)
         self.seconds.setText(difference.second.description)
-
         
     }
 }
